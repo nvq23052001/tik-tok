45b3b8350e80e80f6e2ab3e5037e56b2946fb947
nvquyet <nvquyet23052001@gmail.com>
2022-05-09 22:13:19 +0700

    merge header

--- src/components/Layout/DefaultLayout/index.jsx
+++ src/components/Layout/DefaultLayout/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Header from './Header';
+import Header from '~/components/Layout/components/Header';
 import Sidebar from './Sidebar';
 
 function DefaultLayout({ children }) {
--- src/components/Layout/HeaderOnly/index.jsx
+++ src/components/Layout/HeaderOnly/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Header from './Header';
+import Header from '~/components/Layout/components/Header';
 
 function DefaultLayout({ children }) {
   return (
